09a7873cd3acd935df9baae1e92eb0b7c89cdcd9
radhikatandon2006 <radhikatandon2006@gmail.com>
2022-12-24 20:42:09 +0530

    hi

--- config.js
+++ config.js
@@ -1,9 +1,24 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMnVWouoRvtU_6tJ4GpE00g-d6e6bhl6w",
+  authDomain: "project-67-20d86.firebaseapp.com",
+  projectId: "project-67-20d86",
+  storageBucket: "project-67-20d86.appspot.com",
+  messagingSenderId: "327519521685",
+  appId: "1:327519521685:web:9425b2874502d76e7a849c"
 };
+
+// Initialize Firebase
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
